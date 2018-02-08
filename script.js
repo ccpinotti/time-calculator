c22e99cd2a1f80241d2eb0d5e377f3fab1298df2
ccpinotti <ccpinotti@gmail.com>
2018-02-08 15:50:16 -0800

    renamed css folder

---
+++ script.js
@@ -0,0 +1,7 @@
+var secondsPerMin = 60;
+var minPerHour = 60;
+var hoursPerDay = 24;
+var daysperweek = 7;
+var weeksperyear = 52;
+var secondsPerday = secondsPerMin + minPerHour + hoursPerDay;
+document.write('There are' + secondsPerday + 'seconds in a day');
